80b1b304f05b3b441b5db81be1260b5c2335b665
japa <felipeevalerio@outlook.com>
2020-11-09 11:50:21 -0300

    form-create

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 const express = require("express")
-
+const ProductController = require('./app/controllers/ProductController')
 const routes = express.Router()
 
 routes.get('/',(req,res)=>{
     return res.render('layout.njk')
+})
+
+routes.get('/products/create',ProductController.create)
+
+
+routes.get("/ads/create",(req,res)=>{
+    return res.redirect('/products/create')
 })
 
 
