4dcdf67642ae5b6238d693d1259f8e2a5f7b8386
japa <felipeevalerio@outlook.com>
2020-11-11 19:42:52 -0300

    create

--- public/scripts.js
+++ public/scripts.js
@@ -14,3 +14,39 @@ const Mask = {
 
     }   
 }
+
+const photosUpload = {
+    uploadLimit:6,
+    handleFileInput(event){
+        const {files: fileList} = event.target
+        const {uploadLimit} = photosUpload
+
+        if(fileList.length > uploadLimit){
+            alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return 
+        }
+
+        Array.from(fileList).forEach((file)=>{
+            const reader = new FileReader()
+
+            reader.onload = ()=>{
+                const image = new Image()
+                image.src = String(reader.result)
+
+                const container = document.createElement('div')
+                container.classList.add('photo')
+
+                container.onclick = ()=> alert('OK')
+
+                container.appendChild(image)
+
+                document.querySelector("#photos-preview").appendChild(container)
+
+            }
+            reader.readAsDataURL(file)
+        })
+    }
+
+
+}
