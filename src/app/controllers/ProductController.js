623cbabba1a34ab9bcdeabde4184a5614eb0e4c7
japa <felipeevalerio@outlook.com>
2020-11-17 19:43:11 -0300

    BUCETA DE GERENCIADOR

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -30,9 +30,11 @@ module.exports = {
         const productId = results.rows[0].id
 
 
-        req.files.forEach(files => {
-            await File.create({...file,product_id:productId})
+        const filesPromise = req.files.map(files => {
+            File.create({...files,product_id:productId})
         })
+
+        await Promise.all(filesPromise)
 
         return res.redirect(`/products/${productId}`)
 
@@ -48,7 +50,16 @@ module.exports = {
         results = await Category.all()
         const categories = results.rows
 
-        return res.render('products/edit.njk',{product,categories})
+        results = await Product.files(product.id)
+        
+        let files = results.rows
+        files = files.map(file => ({
+            ...file,
+            src:`${req.protocol}://${req.headers.host}${file.path.replace("public","")}`
+        }))
+
+
+        return res.render('products/edit.njk',{product,categories,files})
     },
     async put(req,res){
         req.body.price = req.body.price.replace(/\D/g,"")
--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -62,5 +62,8 @@ module.exports = {
     },
     delete(id){
         return db.query(`DELETE FROM products WHERE id = $1`,[id])
+    },
+    files(id){
+        return db.query(`SELECT * FROM FILES WHERE product_id = $1`,[id])
     }
 }
